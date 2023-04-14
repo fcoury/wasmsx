bc60061b00dc4fadd3777c03f664f92d81dfc691
Felipe Coury <felipe.coury@gmail.com>
2023-04-14 11:30:42 -0300

    docs: added documentation to typescript code

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,25 @@
+/**
+ * This file contains the implementation of the screen rendering and user interaction
+ * components of an MSX1 emulator.
+ *
+ * The following classes are defined in this file:
+ * - Renderer: Handles the rendering of the emulator screen and data
+ * - App: Manages the interaction between the emulator and the Renderer
+ * - Emulator: Responsible for running the emulator logic and maintaining its state
+ *
+ * The main function initializes the emulator and starts the rendering loop.
+ */
+
 import init, { Machine } from "../pkg/wasmsx.js";
 import ROMS from "./roms.js";
 
 const PROCESSOR_RATE = 1024 * 1024 * 3.5;
+const PALETTE = [
+  // MSX1 color palette
+  0x000000, 0x0000aa, 0x00aa00, 0x00aaaa, 0xaa0000, 0xaa00aa, 0xaa5500,
+  0xaaaaaa, 0x555555, 0x5555ff, 0x55ff55, 0x55ffff, 0xff5555, 0xff55ff,
+  0xffff55, 0xffffff,
+];
 
 class Renderer {
   private screen: HTMLCanvasElement;
@@ -10,6 +28,13 @@ class Renderer {
   private ctx: CanvasRenderingContext2D;
   private screenImageData: ImageData;
 
+  /**
+   * Constructs a new Renderer instance.
+   *
+   * @param {Object} screen - The dimensions of the screen
+   * @param {number} screen.width - The width of the screen
+   * @param {number} screen.height - The height of the screen
+   */
   constructor(screen: { width: number; height: number }) {
     this.screen = document.getElementById("screen") as HTMLCanvasElement;
     this.data = document.getElementById("data") as HTMLDivElement;
@@ -26,6 +51,11 @@ class Renderer {
     );
   }
 
+  /**
+   * Renders the emulator screen.
+   *
+   * @param {Uint8Array} buffer - The screen buffer data
+   */
   public renderScreen(buffer: Uint8Array) {
     const pixels = this.screenImageData.data;
     for (let y = 0; y < 192; y++) {
@@ -34,9 +64,11 @@ class Renderer {
         const color = buffer[colorOffset];
         if (!color) continue;
         const colorBytes = new Uint8Array(4);
-        colorBytes[0] = color;
-        colorBytes[1] = color;
-        colorBytes[2] = color;
+        const paletteColor = PALETTE[color] || 0xffffff;
+
+        colorBytes[0] = (paletteColor >> 16) & 0xff;
+        colorBytes[1] = (paletteColor >> 8) & 0xff;
+        colorBytes[2] = paletteColor & 0xff;
         colorBytes[3] = 255;
         pixels.set(colorBytes, colorOffset * 4);
       }
@@ -44,9 +76,16 @@ class Renderer {
     this.ctx.putImageData(this.screenImageData, 0, 0);
   }
 
+  /**
+   * Renders the internal emulator data for debugging.
+   *
+   * @param {Object} data - The emulator data
+   * @param {number} data.pc - The program counter value
+   * @param {string} data.display_mode - The display mode
+   */
   public renderData(data: { pc: number; display_mode: string }) {
     console.log("pc", data.pc.toString(16), data.display_mode);
-    const pc = document.getElementById("pc");
+    const pc = document.getElementById("data");
     if (pc) {
       pc.innerText = data.pc.toString(16);
     }
@@ -57,15 +96,28 @@ class App {
   private renderer: Renderer;
   private emulator: Emulator;
 
+  /**
+   * Constructs a new App instance.
+   * @param {Renderer} renderer - The Renderer instance
+   * @param {Emulator} emulator - The Emulator instance
+   */
   constructor(renderer: Renderer, emulator: Emulator) {
     this.renderer = renderer;
     this.emulator = emulator;
   }
 
+  /**
+   * Handles keyDown events.
+   * @param {number} key - The key code
+   */
   public keyDown(key: number) {
     this.emulator.keyDown(key);
   }
 
+  /**
+   * Handles a single frame of the emulator.
+   * @param {number} dt - The delta time since the last frame
+   */
   public frame(dt: number) {
     if (dt > 0.2) {
       // console.log(`${dt} seconds behind`);
@@ -83,12 +135,20 @@ class Emulator {
   private timeBudget: number;
   private machine: Machine;
 
+  /**
+   * Constructs a new Emulator instance.
+   * @param {Machine} machine - The Machine instance from the wasm module
+   */
   constructor(machine: Machine) {
     this.machine = machine;
     this.timeBudget = 0;
     this.keys = {};
   }
 
+  /**
+   * Runs the emulator for a specified duration.
+   * @param {number} dt - The delta time since the last run
+   */
   public run(dt: number) {
     this.timeBudget += dt;
     const cycles = Math.floor(this.timeBudget * PROCESSOR_RATE);
@@ -100,18 +160,26 @@ class Emulator {
     }
   }
 
-  public getScreen() {
+  /**
+   * Returns the screen buffer data.
+   * @returns {Uint8Array} The screen buffer data
+   */
+  public getScreen(): Uint8Array {
     return this.machine.screen();
   }
 
-  public getData() {
+  /**
+   * Returns the emulator debug data.
+   * @returns {Object} The emulator data
+   */
+  public getData(): { pc: number; display_mode: string } {
     return { pc: this.machine.pc, display_mode: this.machine.display_mode };
   }
 
-  public screen() {
-    // return this.machine.screen();
-  }
-
+  /**
+   * Handles keyDown events.
+   * @param {number} key - The key code
+   */
   public keyDown(key: number) {
     // this.machine.key_down(key);
   }
