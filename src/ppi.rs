6f658dab67099c4b48b5c97f98b8e86556a0c07d
Felipe Coury <felipe.coury@gmail.com>
2023-04-15 12:48:03 -0300

    feat: initial keyboard processing on the PPI

--- src/ppi.rs
+++ src/ppi.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 use serde::{Deserialize, Serialize};
-use tracing::info;
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
 pub struct Ppi {
@@ -35,49 +34,44 @@ impl Ppi {
         // TODO: psg.set_pulse_signal((register_c & 0xa0) > 0);
     }
 
-    fn update_caps_led(&self) {
-        // TODO leds_socket.led_state_changed(0, (~registerC & 0x40) >> 6);
-    }
-
     pub fn read(&mut self, port: u8) -> u8 {
         match port {
             0xA8 => {
                 // get primary slot config
-                // info!(
-                //     "[PPI] [RD] [PrimarySlot] [{:02X}] = {:02X}",
-                //     port, self.primary_slot_config,
-                // );
+                tracing::info!(
+                    "[PPI] [RD] [PrimarySlot] [{:02X}] = {:02X}",
+                    port,
+                    self.primary_slot_config,
+                );
                 self.primary_slot_config
             }
             0xA9 => {
-                // info!(
-                //     "[PPI] [RD] [KeybordPort] [{:02X}] = {:02X}",
-                //     port, self.register_b
-                // );
-                self.register_b
+                tracing::info!(
+                    "[PPI] [RD] [KeybordPort] [{:02X}] = {:02X}",
+                    port,
+                    self.register_b
+                );
+                self.read_keyboard()
             }
             0xAA => {
-                // returns register and flags
-                // var mod = registerC ^ val;
-                // if (!mod) return;
-                // registerC = val;
-                // if (mod & 0x0f) updateKeyboardConfig();
-                // if (mod & 0xa0) updatePulseSignal();
-                // if (mod & 0x40) updateCapsLed();
-
-                // info!(
-                //     "[PPI] [RD] [Register C ] [{:02X}] = {:02X}",
-                //     port, self.register_c
-                // );
+                tracing::info!(
+                    "[PPI] [RD] [Register C ] [{:02X}] = {:02X}",
+                    port,
+                    self.register_c
+                );
                 self.register_c
             }
             0xAB => {
-                // info!("[PPI] [RD] [IgnoredPort] [{:02X}] = {:02X}", port, 0xFF);
                 // ignored output port
+                tracing::info!("[PPI] [RD] [AB IgnoredP] [{:02X}] = {:02X}", port, 0xFF);
                 0xFF
             }
             _ => 0xFF,
         }
+    }
+
+    pub fn read_keyboard(&self) -> u8 {
+        0x00
     }
 
     pub fn write(&mut self, port: u8, value: u8) {
@@ -92,12 +86,16 @@ impl Ppi {
                 // info!("[PPI] [WR] [IgnoredPort] [{:02X}] = {:02X}", port, value);
             }
             0xAA => {
-                // info!("[PPI] [WR] [PpiControl1] [{:02X}] = {:02X}", port, value);
-                let mode = self.register_c ^ value;
-                if mode == 0 {
+                let modf = self.register_c ^ value;
+                if modf == 0x00 {
                     return;
                 }
                 self.register_c = value;
+
+                if modf & 0x0f != 0 {
+                    self.update_keyboard_config();
+                }
+
                 // var bit = (val & 0x0e) >>> 1;
                 // if ((val & 0x01) === 0) registerC &= ~(1 << bit);
                 // else registerC |= 1 << bit;
@@ -107,22 +105,44 @@ impl Ppi {
                 // else if (bit === 6) updateCapsLed();
             }
             0xAB => {
-                // info!("[PPI] [WR] [PpiControl2] [{:02X}] = {:02X}", port, value);
                 let bit = (value & 0x0e) >> 1;
+                tracing::info!(
+                    "[PPI] [WR] [Port AB    ] [{:02X}] = {:02X} bit {}",
+                    port,
+                    value,
+                    bit
+                );
+                //       info!("[PPI] [WR] [PpiControl2] [{:02X}] = {:02X}", port, value);
                 if (value & 0x01) == 0 {
                     self.register_c &= !(1 << bit);
                 } else {
                     self.register_c |= 1 << bit;
                 }
 
-                // match bit {
-                //     0..=3 => self.update_keyboard_config(),
-                //     5 | 7 => self.update_pulse_signal(),
-                //     6 => self.update_caps_led(),
-                //     _ => (),
-                // }
+                match bit {
+                    0..=3 => self.update_keyboard_config(),
+                    // 5 | 7 => self.update_pulse_signal(),
+                    6 => self.update_caps_led(),
+                    _ => (),
+                }
             }
             _ => (),
         }
+    }
+
+    fn update_keyboard_config(&mut self) {
+        self.keyboard_row_selected = self.register_c & 0x0f;
+        tracing::info!("[PPI] Keyboard row: {}", self.keyboard_row_selected);
+    }
+
+    fn update_caps_led(&mut self) {
+        tracing::info!(
+            "[PPI] CapsLed: {}",
+            if (self.register_c & 0x40) == 0 {
+                "ON"
+            } else {
+                "OFF"
+            }
+        );
     }
 }
